2700971b1f1fb1f944f8897c7f29753eea189c01
mkabilan19 <m.kabilan@iitg.ac.in>
2021-07-13 10:37:43 +0530

    adding npx files

--- README.md
+++
@@ -1,1 +0,0 @@
-# Aamzon-site
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAUYkFuuWXgIuIPGctxpLeDib-CdPJAUJA",
+  authDomain: "clone-18c26.firebaseapp.com",
+  projectId: "clone-18c26",
+  storageBucket: "clone-18c26.appspot.com",
+  messagingSenderId: "511835995792",
+  appId: "1:511835995792:web:4cef32866cb165e41fa215",
+  measurementId: "G-8YXGMG1C0S"
+};
