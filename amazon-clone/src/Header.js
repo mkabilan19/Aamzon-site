4ee6604e2a875fae600967fbce3e3497a03f6fc5
mkabilan19 <m.kabilan@iitg.ac.in>
2021-07-13 11:36:28 +0530

    finished nav section

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+
 
 function Header() {
   return (
-    <div className="header">
-      <img className="header__logo" src='https://pngimg.com/uploads/amazon/amazon_PNG25.png'></img>
+  <div className="header">
+      <img className="header__logo" src='https://pngimg.com/uploads/amazon/amazon_PNG25.png' alt="#"></img>
 
 
     <div 
-    className="header__search">
+      className="header__search">
       <input
        className="header__searchInput"
        type="text" />
+       <SearchIcon className="header__searchIcon" />
     </div>
     
     <div className="header__nav">
@@ -47,11 +52,15 @@ function Header() {
           Prime
         </span>
       </div>
-    </div>
+      
+      <div className="header__optionBasket">
+        <ShoppingBasketIcon />
+        <span className="header__optionLineTwo header__basketCount">0</span>
+      </div>
+     
 
-
-
-    </div>
+     </div>
+  </div>
   )
 }
 
