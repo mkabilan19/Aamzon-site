4eb9916c906e89a130bd7f64f4a65966fd5e6cee
mkabilan19 <m.kabilan@iitg.ac.in>
2021-07-13 21:24:19 +0530

    finished checkout page

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -1,12 +1,32 @@
 import React from "react";
 import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+
 
 function Subtotal() {
   return (
     <div className="subtotal">
-      
+     <CurrencyFormat
+      renderText={(value) => (
+        <>
+          <p>
+            Subtotal (0 items) : <strong>0</strong>
+          </p>
+          <small className="subtotal__gift">
+            <input type="checkbox" /> this order
+            conatins a gift
+          </small>
+        </>
+      )}
+      decimalScale={2}
+      value={0}
+      displayType={"text"}
+      thousandSeprator={true}
+      prefix={"$"}
+     />
+     <button>Proceed to Checkout</button>
     </div>
-  )
+  );
 }
 
 export default Subtotal
