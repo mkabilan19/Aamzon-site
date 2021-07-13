c98027aaaf11b06a73fc984b9309ef5e62b9bfec
mkabilan19 <m.kabilan@iitg.ac.in>
2021-07-13 14:46:40 +0530

    FINALLY FINISHED HOME PAGE

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,17 +2,25 @@ import React from "react";
 import './App.css';
 import Header from './Header';
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
-    <div className="app">
-
-    {/* header */}
-    <Header />
-    {/* home */}
-    <Home />
-
-    </div>
+    <Router>
+      <div className="app">
+      <Switch>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
