884ff74fd159d2983e2b4ccb40762da4f84bc31f
mkabilan19 <m.kabilan@iitg.ac.in>
2021-07-14 00:14:29 +0530

    done the checkout left side page

---
+++ amazon-clone/src/reducer.js
@@ -0,0 +1,27 @@
+// this is the data layer 
+
+export const initialState = {
+  basket: [],
+};
+
+// selector 
+// this is basically adding the sum in for loop and returning the sum
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount , 0);
+
+
+const reducer = (state, action) => {
+   console.log(action);
+  switch(action.type){
+    case'ADD_TO_BASKET':
+        return{
+          ...state,
+          basket: [...state.basket, action.item],
+        };
+
+     default:
+       return state;   
+  }
+};
+
+export default reducer;
