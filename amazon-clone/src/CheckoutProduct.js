a24afef126c429cc753573a6619f7213ea158352
mkabilan19 <m.kabilan@iitg.ac.in>
2021-07-14 16:58:29 +0530

    finished all work in checkout page

--- amazon-clone/src/CheckoutProduct.js
+++ amazon-clone/src/CheckoutProduct.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './CheckoutProduct.css'
-
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating}) {
+  const[{basket}, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    // removing the item
+    dispatch({
+      type: 'REMOVE_FROM_BASKET',
+      id: id,
+    })
+  }
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} />
@@ -20,7 +29,7 @@ function CheckoutProduct({ id, image, title, price, rating}) {
                   <p>⭐</p>
               ))}
          </div>
-         <button>Remove the Product</button>
+         <button onClick={removeFromBasket}>Remove the Product</button>
       </div>
     </div>
   )
