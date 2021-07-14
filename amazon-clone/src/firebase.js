b51e2015caabb49aec1df8f4510d1bdbf9ac8058
mkabilan19 <m.kabilan@iitg.ac.in>
2021-07-14 19:25:35 +0530

    doing the auth work

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,4 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyAUYkFuuWXgIuIPGctxpLeDib-CdPJAUJA",
   authDomain: "clone-18c26.firebaseapp.com",
@@ -8,3 +11,10 @@ const firebaseConfig = {
   appId: "1:511835995792:web:4cef32866cb165e41fa215",
   measurementId: "G-8YXGMG1C0S"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth};
